cb7707a3d5e5c6607083300725773ea7d135b317
Michael Kevin <michaelkevin3112@gmail.com>
2025-08-29 10:05:50 +0700

    Update assignment.c

--- assignment.c
+++ assignment.c
@@ -1,3 +1,9 @@
-﻿printf("My Name is Michael Kevin Descvio Wando Taher");
+#include <stdio.h>
+
+int main()
+{
+printf("My Name is Michael Kevin Descvio Wando Taher");
 printf("I Like C");
 
+  return 0;
+}
